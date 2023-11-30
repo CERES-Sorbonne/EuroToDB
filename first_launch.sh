aa6942a43e1850c137da38a250f9c2df8a9523c8
Marceau <git@marceau-h.fr>
2023-11-30 10:32:03 +0100

    typo

--- first_launch.sh
+++ first_launch.sh
@@ -5,4 +5,4 @@ source venv/bin/activate || exit 1
 pip install -U pip setuptools wheel || exit 1
 pip install -r requirements.txt || exit 1
 
-python from_env_to_creds.py || exit 1
+python3.11 from_env_to_creds.py || exit 1
