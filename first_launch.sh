dc49c2bde19f6710df4d85fefd6d8541e59f6b4f
Marceau <git@marceau-h.fr>
2023-11-30 10:24:59 +0100

    first launch

--- first_launch.sh
+++ first_launch.sh
@@ -4,3 +4,5 @@ python3.11 -m venv venv
 source venv/bin/activate || exit 1
 pip install -U pip setuptools wheel || exit 1
 pip install -r requirements.txt || exit 1
+
+python from_env_to_creds.py || exit 1
