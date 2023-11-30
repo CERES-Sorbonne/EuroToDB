6387a103d7bda4a101148f901e910b8aba451c68
Marceau <git@marceau-h.fr>
2023-11-30 10:03:19 +0100

    Typo again

--- start_watchdog.sh
+++ start_watchdog.sh
@@ -7,6 +7,6 @@ set +a
 env | grep -E '^(TZ|POSTGRES|HOST|FOLDER|CREDSFILE|CONTAINER)' || exit 1
 
 source venv/bin/activate || exit 1
-python ElChienDeGarde.py "$CREDS_FILE" "$FOLDER_TO_WATCH" || exit 1
+python ElChienDeGarde.py "$CREDSFILE" "$FOLDER_TO_WATCH" || exit 1
 
 # EOF
